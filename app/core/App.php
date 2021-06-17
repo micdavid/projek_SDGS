f915419303b3c55e4d3b72c0d3894cf22e37d70b
wnykhza <fathorrosi1777@gmail.com>
2021-06-17 16:08:42 +0700

    broken link

--- app/core/App.php
+++ app/core/App.php
@@ -1,4 +1,5 @@
 <?php
+
 class App
 {
     protected $controller   = 'Home';
@@ -8,8 +9,8 @@ class App
     public function __construct()
     {
         $url    = $this->parseURL();
-        // * controller
-        if (isset($url[0])) {
+        
+        if (isset($url[0])) {                   
             if (file_exists('../app/controllers/' . $url[0] . '.php')) {
                 $this->controller = $url[0];
                 unset($url[0]);
@@ -18,7 +19,6 @@ class App
         require_once '../app/controllers/' . $this->controller . '.php';
         $this->controller = new $this->controller;
 
-        // * method
         if (isset($url[1])) {
             if (method_exists($this->controller, $url[1])) {
                 $this->method = $url[1];
@@ -26,12 +26,10 @@ class App
             }
         }
 
-        // * params
         if (!empty($url)) {
             $this->params = array_values($url);
         }
 
-        // * jalankan semua
         call_user_func_array([$this->controller, $this->method], $this->params);
     }
 
