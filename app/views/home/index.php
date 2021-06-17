5d2c442c342bae9af4576be220990b347ee1a086
micdavid <17michaelsimon@gmail.com>
2021-06-17 13:26:34 +0700

    header

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -64,7 +64,7 @@
                 <ul>
                     <li class="active"><a href="index.php">Home</a></li>
                     <li><a href="#about">About</a></li>
-                    <li><a href="#services">Services</a></li>
+                    <li><a href="#services">Charity</a></li>
                     <li><a href="#team">Team</a></li>
                     <li><a href="#contact">Contact</a></li>
 
